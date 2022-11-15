75c12d7fa2fd6df6fa01d6996a20897d977950d8
TatyanaChi <tischenko3004@gmail.com>
2022-11-15 23:15:27 +0100

    add task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsContainerEl = document.querySelector("#ingredients");
+
+const elements = ingredients.map((ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = ingredient;
+  itemEl.classList.add("item");
+
+  return itemEl;
+});
+
+console.log(elements);
+
+ingredientsContainerEl.append(...elements);
