0ea29beff8278fb04dec28cf863f721270478ec5
TatyanaChi <tischenko3004@gmail.com>
2022-11-16 16:09:21 +0100

    add task-04

---
+++ js/task-04.js
@@ -0,0 +1,24 @@
+let counterValue = 0;
+
+const actions = document.querySelector("#counter");
+
+const actionsValue = document.querySelector("#value");
+
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+console.log(decrementBtn.dataset.action);
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+console.log(incrementBtn.dataset.action);
+
+function onTargetButtonDecrement(event) {
+  event.currentTarget.value -= 1;
+  actionsValue.textContent = event.currentTarget.value;
+}
+
+function onTargetButtonIncrement(event) {
+  event.currentTarget.value += 1;
+  console.log(actionsValue.textContent);
+  actionsValue.textContent = event.currentTarget.value;
+}
+
+decrementBtn.addEventListener("click", onTargetButtonDecrement);
+incrementBtn.addEventListener("click", onTargetButtonIncrement);
