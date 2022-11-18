4b1c5d5e707861a9ee70735b449d972b97d8aa4e
TatyanaChi <tischenko3004@gmail.com>
2022-11-18 20:11:10 +0100

    add task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (email.length === 0 || password.length === 0) {
+    alert("Не все поля заполнены. Дополните, пожалуйста.");
+    return;
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+  console.log(formData);
+  form.reset();
+}
