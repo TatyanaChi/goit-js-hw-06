e370a1dd5ca1c2246597179781318ebe3ff6f2a5
TatyanaChi <tischenko3004@gmail.com>
2022-11-18 21:36:30 +0100

    add task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const body = document.querySelector("body");
+const changeColor = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", onBtnChangeColor);
+
+function onBtnChangeColor(event) {
+  changeColor.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+}
